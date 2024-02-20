aa451dacb475b6c5939c1a1998b6e0dd8c7eb96f
SeungEunChun <1000tmddms@gmail.com>
2024-02-20 17:26:49 +0900

    Movie라는 독자적인 컴포넌트를 만들어서 Typescript로 타입 설정 후 map 함수를 사용하여 데이터의 갯수만큼 노출+각각의 css 모듈을 만들어서 레이아웃 수정

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,23 +1,28 @@
-import Link from "next/link";
+import Movie from "../../components/movie";
+import styles from "../../styles/home.module.css";
 
 export const metadata = {
-    title: 'Home',
-
-}
-
+    title: "Home",
+};
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 async function getMovies() {
-    await new Promise((res => setTimeout(res, 1000)))
-    const res = await fetch(API_URL);
-    const json = await res.json();
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
+    const response = await fetch(API_URL);
+    const json = await response.json();
     return json;
 }
-export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
-
-export default async function Myhome() {
+export default async function HomePage() {
     const movies = await getMovies();
-    return <div>
-        <ul>
-            {movies.map(movie => <li key={movie.id}><Link href={`/movies/${movie.id}`}>{movie.title}</Link></li>)}
-        </ul>
-    </div>
+    return (
+        <div className={styles.container}>
+            {movies.map((movie) => (
+                <Movie
+                    key={movie.id}
+                    id={movie.id}
+                    poster_path={movie.poster_path}
+                    title={movie.title}
+                />
+            ))}
+        </div>
+    );
 }
