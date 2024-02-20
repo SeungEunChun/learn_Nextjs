190bf0ee87f2b5538354dc5ffc5d38d34b911ad5
SeungEunChun <1000tmddms@gmail.com>
2024-02-20 14:25:52 +0900

    async 함수 next에서 만드는법

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,7 +4,17 @@ export const metadata = {
 
 }
 
+async function getMovies() {
+    await new Promise((res => setTimeout(res, 2000)))
+    const res = await fetch(URL);
+    const json = await res.json();
+    return json;
+}
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-export default function Myhome() {
-    return <h1>Hello Nextjs!</h1>
+export default async function Myhome() {
+    const movies = await getMovies();
+    return <div>
+        {JSON.stringify(movies)}
+    </div>
 }
