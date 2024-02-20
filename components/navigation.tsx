6e8ecbb4bca9c50af6f4542cb59daddb4e2fad80
SeungEunChun <1000tmddms@gmail.com>
2024-02-20 12:03:30 +0900

    간단한 네비게이션 바 구현

---
+++ components/navigation.tsx
@@ -0,0 +1,7 @@
+import Link from "next/link"
+export default function Navigation() {
+    return <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/about-us">About-us</Link></li>
+    </ul>
+}
