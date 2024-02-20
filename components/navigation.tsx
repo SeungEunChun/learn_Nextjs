126bb8be856a5e58fc56b8a786de7105faedc050
SeungEunChun <1000tmddms@gmail.com>
2024-02-20 16:31:20 +0900

    module.css 사용법/ 해당 파일을 자바스크립트처럼 적용하고싶은 곳에 import하고 클래스 안에 import한이름.클래스명 써준다

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,12 +2,13 @@
 
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import styles from '../styles/navigation.module.css'
 export default function Navigation() {
     const path = usePathname();
-    return <nav>
+    return <nav className={styles.nav}>
         <ul>
-            <li><Link href="/">Home</Link>{path === "/" ? "🥊" : ""}</li>
-            <li><Link href="/about-us">About-us</Link> {path === "/about-us" ? "🥊" : ""}</li>
+            <li><Link href="/">Home</Link>{path === "/" ? "🔷" : ""}</li>
+            <li><Link href="/about-us">About-us</Link> {path === "/about-us" ? "🔷" : ""}</li>
         </ul>
     </nav>
 }
