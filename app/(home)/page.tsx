6a2793f51c1635188fde69c2dc89a9971556ed4f
SeungEunChun <1000tmddms@gmail.com>
2024-02-20 12:44:38 +0900

    메타데이터가 중첩되지 않고 병합됨을 코드로 구현

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,10 @@
+
+export const metadata = {
+    title: 'Home',
+
+}
+
+
 export default function Myhome() {
     return <h1>Hello Nextjs!</h1>
 }
--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,3 +1,9 @@
+
+export const metadata = {
+    title: 'About-us',
+
+}
+
 export default function AboutUs() {
     return <h2>About-Us</h2>
 }
