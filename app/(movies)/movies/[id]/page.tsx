66771a2493f978b7953dfe5938b6072d3af303fc
SeungEunChun <1000tmddms@gmail.com>
2024-02-20 14:48:49 +0900

    Promise.all 을 활용해 두 async 함수가 순차적으로 실행되는 것이 아닌 동시에 병렬적으로 수행

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,10 +1,24 @@
 import { API_URL } from "../../../(home)/page"
 async function getMovie(id: string) {
+    console.log(`Fetching Movie :  ${Date.now()}`) // 영화 패치 시간
+    await new Promise((res) => setTimeout(res, 2000))
     const res = await fetch(`${API_URL}/${id}`);
     return res.json();
 }
 
+async function getVideos(id: string) {
+    console.log(`Fetching Video : ${Date.now()}`) //영화 비디오 패치 시간
+    await new Promise((res) => setTimeout(res, 2000))
+    const res = await fetch(`${API_URL}/${id}/videos`);
+    return res.json();
+}
+
+
+
+
 export default async function MovieDetail({ params: { id } }: { params: { id: string } }) {
-    const movie = await getMovie(id);
-    return <h1>{movie.title}</h1>
+    console.log("fetch start")
+    const [movies, videos] = await Promise.all([getMovie(id), getVideos(id)])
+    console.log("fetch complete")
+    return <h1>{movies.title}</h1>
 }
