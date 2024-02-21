9e36d5e91e61e11fc479a8832803def138d2483b
SeungEunChun <1000tmddms@gmail.com>
2024-02-21 15:16:58 +0900

    함수명 재정의

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -7,14 +7,14 @@ export const metadata = {
     title: "Home",
 };
 export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
-export async function getMovies() {
+export async function getMovie() {
     // await new Promise((resolve) => setTimeout(resolve, 1000));
     const response = await fetch(API_URL);
     const json = await response.json();
     return json;
 }
 export default async function HomePage() {
-    const movies = await getMovies();
+    const movies = await getMovie();
     return (
         <div className={styles.container}>
 
