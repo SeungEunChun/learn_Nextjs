e6b76e53f0734217de7a690c24eca6e04ce42313
SeungEunChun <1000tmddms@gmail.com>
2024-02-21 11:51:32 +0900

    iframe 사용법 수정! 복습!

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/movie-videos.module.css'
 
 async function getVideos(id: string) {
     console.log(`Fetching Video : ${Date.now()}`) //영화 비디오 패치 시간
-    await new Promise((res) => setTimeout(res, 3000))
+    await new Promise((res) => setTimeout(res, 1000))
     const res = await fetch(`${API_URL}/${id}/videos`);
     return res.json();
 }
@@ -15,7 +15,7 @@ export default async function MovieVideos({ id }: { id: string }) {
     return <div className={styles.container}>
         {
             videos.map((video) => (
-                <iframe key={video.id} src={`https://youtube/embed/${video.key}`} title={video.name} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                <iframe key={video.id} src={`https://youtube.com/embed/${video.key}`} title={video.name} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen />
             ))
         }
