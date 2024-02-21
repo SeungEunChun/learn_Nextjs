eb7cc13639ef4b1b09a81d28cd951f719fe29b7b
SeungEunChun <1000tmddms@gmail.com>
2024-02-21 16:28:36 +0900

    prefetch

--- components/movie.tsx
+++ components/movie.tsx
@@ -21,7 +21,7 @@ export default function Movie({ title, id, poster_path }: Movieprops) {
     return (
         <div className={styles.movie}>
             <img src={poster_path} alt={title} onClick={handleclick} />
-            <Link href={`/movies/${id}`}>{title}</Link>
+            <Link prefetch href={`/movies/${id}`}>{title}</Link>
         </div>
     )
 }
